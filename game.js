04e0add80050f71b40f807398593f9facee4dcba
Llouisechung <louisechung24@gmail.com>
2025-02-05 11:54:17 +0100

    re- exam game

--- game.js
+++ game.js
@@ -1,512 +1,365 @@
-let x = 100;
-let y = 100;
-let bearX = 0;
-let bearY = 290;
-let goalX = x + 630;
-let goalY = y + 160;
-let goalWidth = 60;
-let goalHeight = 75;
-
+let startX = 400;
+let startY = 570;
 let gameState = "start";
+let startLives = 2;
+let gameLives = startLives;
+let score = 0;
+
+
+//line 9 of the code is added by courtesy of Stefan La
+let logSafeAreaBoarderWidth = 10;
 
 function setup() {
   createCanvas(800, 600);
+  background(100);
   noStroke();
-  //grass background
-  background(207, 196, 97);
-
-  initBerries();
 }
 
-window.setup = setup;
-
-//bear hitbox collision detector (got help a friend)
-const bearOffsetX = 24;
-const bearOffsetY = 0;
-
-const bearHitboxX = 28;
-const bearHitboxY = 28;
-
-const safeTiles = [0, 2, 3];
-const blockedTiles = [1];
-
-const tileSize_X = 32;
-const tileSize_Y = 32;
-
-//map 2D arrays
-
-let map = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 0, 0, 4, 0, 0, 0, 1],
-  [1, 0, 0, 4, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 0, 1, 1, 1, 5, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1],
-  [1, 4, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 4, 0, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 4, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1],
-  [1, 1, 1, 1, 1, 4, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1],
-  [1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 4, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-  [2, 0, 0, 0, 0, 0, 0, 1, 4, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 3],
-  [1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1],
-  [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 4, 1, 1, 1, 1],
-  [1, 0, 0, 1, 1, 1, 0, 5, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 1, 0, 1, 4, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 4, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 4, 1],
-  [1, 4, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 5, 0, 1],
-  [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 1, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-];
-
-//classes
-class Bear {
-  constructor(x, y) {
-    this.x = x;
+class character {
+  constructor(x, y, width, height) {
+    this.x = x;  
     this.y = y;
+    this.width = width;
+    this.height = height;
   }
 
-  //bear movement in x- positions
-  moveX(x) {
-    const newX = this.x + x;
-    if (this.colide(newX, this.y) === false) {
-      this.x = newX;
-    }
-  }
-
-  //bear movement in y- positions
-  moveY(y) {
-    const newY = this.y + y;
-
-    if (this.colide(this.x, newY) === false) {
-      this.y = newY;
-    }
-  }
-
-  //calculate where the hitbox is in the map (got help from a friend)
-  colide(x, y) {
-    let c1 = map[Math.floor(y / 32)][Math.floor(x / 32)];
-    let c2 = map[Math.floor(y / 32)][Math.floor((x + bearHitboxX) / 32)];
-    let c3 = map[Math.floor((y + bearHitboxY) / 32)][Math.floor(x / 32)];
-    let c4 =
-      map[Math.floor((y + bearHitboxY) / 32)][
-        Math.floor((x + bearHitboxX) / 32)
-      ];
-
-    if ([c1, c2, c3, c4].includes(2)) {
-      console.log("start!");
-    }
-    
-    if ([c1, c2, c3, c4].includes(3)) {
-      gameState = "GameSuceeded";
-      console.log("you have reached goal!");
-    }
-
-    if ([c1, c2, c3, c4].includes(5)) {
-      console.log("berry");
-      for (let berry of berries) {
-        berries.splice(berries.indexOf(berry), 1); 
-      }
-    } 
- 
-    if ([c1, c2, c3, c4].includes(4)) {
-    
-      gameState = "gameOver";
-      return true;
-    }
-    return [c1, c2, c3, c4].some((value) => blockedTiles.includes(value));
-
-
-  }
   draw() {
     push();
-    fill(0, 255, 0);
-    rect(this.x, this.y, bearHitboxX, bearHitboxY);
-    strokeWeight(20);
-    angleMode(DEGREES);
-    pop();
-
-    //bear body light
-    push();
-    translate(this.x - 8 + bearOffsetX, this.y + 15 + bearOffsetY);
-    fill(139, 86, 33);
-    ellipse(0, 0, 34, 38);
-    pop();
-
-    //bear arms
-    push();
-    translate(this.x + bearOffsetX, this.y + bearOffsetY);
-    fill(77, 47, 17);
-    ellipse(0, 0, 16, 11);
-    pop();
-
-    push();
-    translate(this.x + bearOffsetX, this.y + 29 + bearOffsetY);
-    fill(77, 47, 17);
-    ellipse(0, 0, 16, 11);
-    pop();
-
-    //bear head
-    push();
-    translate(this.x - 14 + bearOffsetX, this.y + 15 + bearOffsetY);
-    fill(102, 51, 0);
-    ellipse(0, 0, 30, 40);
-    pop();
-
-    //bear ears
-    push();
-    translate(this.x - 25 + bearOffsetX, this.y + 4 + bearOffsetY);
-    fill(102, 51, 0);
-    ellipse(0, 0, 15, 15);
-    pop();
-
-    push();
-    translate(this.x - 25 + bearOffsetX, this.y + 26 + bearOffsetY);
-    fill(102, 51, 0);
-    ellipse(0, 0, 15, 15);
-    pop();
-
-    //bear nose
-    push();
-    translate(this.x + bearOffsetX, this.y + 15 + bearOffsetY);
-    fill(0);
-    ellipse(0, 0, 8, 10);
-    pop();
-
-    //bear eyes
-    push();
-    translate(this.x - 10 + bearOffsetX, this.y + 7 + bearOffsetY);
-    fill(0);
-    ellipse(0, 0, 5, 5);
-    pop();
-
-    push();
-    translate(this.x - 10 + bearOffsetX, this.y + 23 + bearOffsetY);
-    fill(0);
-    ellipse(0, 0, 5, 5);
+    fill(154,205,50);
+    square(this.x, this.y, this.width);
     pop();
   }
+
+  move(x, y) {
+    this.x += x;
+    this.y += y;
   }
 
-//instance of bear
-const bear = new Bear(bearX, bearY);
+  
+  update() {
+  // The following 10 lines of the code is added by ourtesy of Stefan La
 
-class bush {
-  constructor(x, y) {
-    push();
-    fill(0, 255, 0);
-    rect(x * tileSize_X, y * tileSize_Y, tileSize_X, tileSize_Y);
-    strokeWeight(20);
-    angleMode(DEGREES);
-    pop();
+  //checking if character moves with the log otherwise character dies
+  let onLog = false;
+  
+  if (this.y < 200 && this.y > 100) {
+   for (let i = 0; i < logs.length; i++) {
+    let currentLog = logs[i];
+
+  if (
+    this.x + this.width > currentLog.x + logSafeAreaBoarderWidth &&
+    this.x < currentLog.x + currentLog.width - logSafeAreaBoarderWidth
+  ) 
+  {
+ //character moves with the log if character is on it
+    this.x -= currentLog.speed;
+    onLog = true;
   }
+ }
+
+ //character dies if fallen into water
+if (!onLog) {
+  console.log("DROWNED");
+  if (gameLives === 0) {
+    gameState = "gameOver";
+  } else {
+    gameLives -= 1;
+    this.x = startX;
+    this.y = startY;
+  }
+ }
+
 }
-
-class ground {
-  constructor(x, y) {
-    push();
-    fill(150, 121, 105);
-    rect(x * tileSize_X, y * tileSize_Y, tileSize_X, tileSize_Y);
-    strokeWeight(20);
-    angleMode(DEGREES);
-    pop();
+  if (keyIsDown(UP_ARROW)) {
+    this.move(0, -5);
   }
-}
-
-class start {
-  constructor(x, y) {
-    push();
-    fill(150, 191, 105);
-    rect(x * tileSize_X, y * tileSize_Y, tileSize_X, tileSize_Y);
-    strokeWeight(20);
-    angleMode(DEGREES);
-    pop();
+  if (keyIsDown(DOWN_ARROW)) {
+    this.move(0, 5);
   }
-}
-
-class goal {
-  constructor(x, y) {
-    push();
-    fill(25, 25, 25);
-    rect(x * tileSize_X, y * tileSize_Y, tileSize_X, tileSize_Y);
-    strokeWeight(20);
-    angleMode(DEGREES);
-    pop();
+  if (keyIsDown(LEFT_ARROW)) {
+    this.move(-5, 0);
   }
-}
+  if (keyIsDown(RIGHT_ARROW)) {
+    this.move(5, 0);
+  } 
 
-class traps {
-  constructor(x, y) {
-    push();
-    fill(255, 0, 0);
-    rect(x * tileSize_X, y * tileSize_Y, tileSize_X, tileSize_Y);
-    strokeWeight(20);
-    angleMode(DEGREES);
-    pop();
-  }
-}
 
-class Berries {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-  show() {
-    push();
-    translate(this.x, this.y);
-    fill(0, 0, 255);
-    ellipse(0, 0, 10, 10);
-    pop();
-    // console.log("im a berry");
-  }
-}
-
-let berry1 = new Berries(100, 100);
-
-let berry2 = new Berries(300, 100);
-
-let berry3 = new Berries(250, 100);
-
-function maze() {
-  for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[i].length; j++) {
-      if (map[i][j] === 0) {
-        new ground(j, i);
-      }
-      if (map[i][j] === 1) {
-        new bush(j, i);
-      }
-      if (map[i][j] === 2) {
-        new start(j, i);
-      }
-      if (map[i][j] === 3) {
-        new goal(j, i);
-      }
-      if (map[i][j] === 4) {
-        new traps(j, i);
-      }
-      if (map[i][j] === 5) {
-        new ground(j, i);
-      }
+    //player gains a point, after 3 points player wins. 
+     if (this.y < 40) {
+      score++;
+      character1.x = startX;
+      character1.y = startY;
+     }
+     if(score === 3) {
+      gameState = "gameSucceeded";
+      character1.x = startX;
+      character1.y = startY;
+      
+     }  
+    
     }
-  }
+ } 
+
+const character1 = new character(350, 550, 50, 50);
+
+function safeGround() {
+  //Safe grounds between the starting and end ground
+  push();
+  fill(100);
+  rect(0, 170, width, 360);
 }
 
-function startScreen() {
-  //background
+function startingGround() {
+  //starting ground
   push();
-  fill(135, 206, 235);
-  rect(0, 0, 800, 600);
-  pop();
 
-  //Game title
-  push();
-  fill(255);
-  textSize(50);
-  textAlign(CENTER, CENTER);
-  textFont("monospace");
-  textStyle(BOLDITALIC);
-  text("BEARBERRY CHASE", 400, 100);
-  pop();
-
-  //push button bar
-  push();
-  fill(135, 136, 265);
-  rect(280, 240, 230, 60, 20);
-  pop();
-
-  //start button text
-  push();
-  fill(255);
-  textSize(32);
-  textAlign(CENTER, CENTER);
-  textFont("helvetica");
-  textStyle(BOLD);
-  text("PLAY", 393, 274);
-  pop();
-
-  //instructions text
-  push();
-  fill(255);
-  textSize(18);
-  textAlign(CENTER, CENTER);
-  textFont("monospace");
-  textStyle(BOLDITALIC);
-  text(
-    "Avoid the lava and collect all the blue berries as fast as possible!",
-    400,
-    400
-  );
+  fill(169, 169, 169);
+  rect(0, height - 100, width, 80);
   pop();
 }
 
+function endingGround() {
+  //goal
+  push();
 
+  fill(169, 169, 169);
+  rect(0, 0, width, 100);
+  pop();
+}
+
+function water() {
+  push();
+  fill(0, 0, 255);
+  rect(0, 100, width, 100);
+  pop();
+}
+
+function startscreen() {
+  push();
+  background(255,211,182);
+  fill(0);
+  textSize(30);
+  text("Skipper Game", 300, 150);
+  pop();
+
+  push();
+  fill(0);
+  textSize(20);
+  text("Use the arrow keys to play. Gain 3 points to win! ", 200, 350);
+  pop();
+
+  push();
+  fill(0);
+  textSize(100);
+  text("🐸", 330,450);
+  pop();
+}
 function gameWin() {
-  //background
   push();
-  fill(135, 206, 235);
-  rect(0, 0, 800, 600);
-  pop();
-
-  //Win feedback text
-  push();
-  fill(255);
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  textFont("monospace");
-  textStyle(BOLDITALIC);
-  text("YOU FOUND ALL THE BERRIES!", 400, 100);
-  pop();
-
-  push();
-  fill(255);
-  textSize(25);
-  textAlign(CENTER, CENTER);
-  textFont("monospace");
-  textStyle(BOLD);
-  text("Can you beat your time next round?", 400, 170);
+  background(220,237,193);
+  fill(0);
+  textSize(30);
+  text("You Succeeded!", 300, 150);
   pop();
 }
 
 function gameLose() {
-  //background
   push();
-  fill(135, 206, 235);
-  rect(0, 0, 800, 600);
-  pop();
-
-  //Lose feedback text
-  push();
-  fill(255);
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  textFont("monospace");
-  textStyle(BOLDITALIC);
-  text("Better luck next time...", 400, 100);
+  background(255,139,148);
+  fill(0);
+  textSize(30);
+  text("You Have Lost", 300, 150);
   pop();
 }
 
-function restart() {
-  //restart button bar
-  push();
-  fill(135, 136, 265);
-  rect(280, 240, 230, 60, 20);
-  pop();
-
-  //restart text
-  push();
-  fill(255);
-  textSize(32);
-  textAlign(CENTER, CENTER);
-  textFont("helvetica");
-  textStyle(BOLD);
-  text("RETRY", 393, 274);
-  pop();
-}
-
-function gamePlay() {
-  let newX = x;
-  let newY = y;
-
-  if (keyIsDown(87)) {
-    // 'W' key
-    bear.moveY(-5);
-  }
-  if (keyIsDown(65)) {
-    // 'A' key
-    bear.moveX(-5);
-  }
-  if (keyIsDown(83)) {
-    // 'S' key
-    bear.moveY(5);
-  }
-  if (keyIsDown(68)) {
-    // 'D' key
-    bear.moveX(5);
+class Button {
+  constructor(x, y, width, height, text) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.text = text;
   }
 
-  //game ends when player is inside goal
-  if (
-    bear.x + bear.width > goalX &&
-    bear.x < goalX + goalWidth &&
-    bear.y + bear.height > goalY &&
-    bear.y < goalY + goalHeight
-  ) {
-    gameState = "gameSucceeded";
+  isPressed(x, y) {
+    if (
+      x > this.x &&
+      x < this.x + this.width &&
+      y > this.y &&
+      y < this.y + this.height
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  draw() {
+    push();
+    translate(this.x, this.y);
+    noStroke();
+    fill(255);
+    rect(0, 0, this.width, this.height, this.height);
+
+    //define text
+    fill(255, 0, 0);
+    textSize(this.height / 2);
+    textAlign(CENTER, CENTER);
+    text(this.text, this.width - 100, this.height / 2);
+    pop();
   }
 }
 
-function mouseClicked() {
-  //click on start botton
-  if (
-    gameState === "start" &&
-    mouseX > 280 &&
-    mouseX < 510 &&
-    mouseY > 240 &&
-    mouseY < 300
-  ) {
-    gameState = "playing";
+const startButton = new Button(300, 200, 200, 50, "Press to start");
+const restartButton = new Button(300, 200, 200, 50, "Restart Game");
 
-    x = 100;
-    y = 100;
-  } else if (
-    (gameState === "gameOver" || gameState === "gameSucceeded") &&
-    mouseX > 280 &&
-    mouseX < 510 &&
-    mouseY > 240 &&
-    mouseY < 300
-  ) {
-    gameState = "playing";
-
-    x = 100;
-    y = 100;
+class Obstacle {
+  constructor(x, y, width, height, r, g, b, speed, addSpeed) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.speed = speed;
+    this.addSpeed = addSpeed;
   }
-}
 
-window.mouseClicked = mouseClicked;
+  update() {
+    //moved the obstacle in x-position
+    if (this.x <= 800) {
+      this.x = this.x + (this.speed + this.addSpeed);
+    } else if (this.x > 800) {
+      this.x = 0 - 100;
+    }
 
-let berries = [];
-function initBerries() {
-  for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[i].length; j++) {
-      if (map[i][j] === 5) {
-        berries.push(new Berries(j * tileSize_X, i * tileSize_Y));
+    //checks if character is colliding with obstacle
+    if (
+      character1.x < this.x + this.width &&
+      character1.x + character1.width > this.x &&
+      character1.y < this.y + this.height &&
+      character1.y + character1.height > this.y
+    ) {
+      // checks when gamelife reached zero you lose. Character starting point whenever you dies
+      if (gameLives === 0) {
+        gameState = "gameOver";
+      } else {
+        gameLives -= 1;
+        character1.x = startX;
+        character1.y = startY;
       }
     }
   }
+
+  draw() {
+    push();
+    fill(this.r, this.g, this.b);
+    rect(this.x, this.y, this.width, this.height);
+    pop();
+  }
+}
+
+const obstacle1 = new Obstacle(50, 400, 100, 100, 217, 59, 58 , 8, 1);
+const obstacle2 = new Obstacle(50, 200, 100, 100, 235 , 121 ,61, 5, 1);
+const obstacle3 = new Obstacle(480, 200, 100, 100, 197, 228 ,230, 5, 1);
+const obstacle4 = new Obstacle(480, 400, 100, 100, 43 ,38 ,34, 8, 1);
+
+//obstacles array
+let obstacles = [obstacle1, obstacle2, obstacle3, obstacle4];
+
+class Log extends Obstacle {
+  constructor(x, y, width, height, speed, addSpeed) {
+    super(x, y, width, height, 139, 69, 19, speed, addSpeed);
+  }
+  update() {
+
+    if (this.x >= -100) {
+      this.x = this.x - (this.speed + this.addSpeed);
+    } else if (this.x < -100) {
+      this.x = 800;
+    }
+    }
+  }
+
+const log1 = new Log(50, 100, 150, 100, 4, 1);
+const log2 = new Log(600, 100, 150, 100, 4, 1);
+
+//logs array
+const logs = [log1, log2];
+
+
+function mousePressed() {
+  //character restart point when return button is pressed.
+  if (startButton.isPressed(mouseX, mouseY)) {
+    gameState = "playing";
+    character1.x = startX;
+    character1.y = startY;
+    gameLives = startLives;
+    score = 0;
+  }
+  
+  if (restartButton.isPressed(mouseX, mouseY)) {
+    gameState = "playing";
+    character1.x = startX;
+    character1.y = startY;
+    gameLives = startLives;
+    score = 0;
+  }
+}
+
+
+function drawLives() {
+  //The following 5 lines is adapted from https://chatgpt.com/share/67a14f4c-3f04-8008-a84e-5b77d81cf43f
+  push();
+  fill(0);
+  textSize(25);
+  text("Lives: " + (gameLives + 1), 30, 30);
+  pop();
+}
+
+function drawScore() {
+  push();
+  fill(0);
+  textSize(25);
+  text("Scores: " + score, 11, 60);
+  pop();
 }
 
 function draw() {
-  background(207, 196, 97);
-  maze(x, y);
-
-  bear.draw();
-
-  if (map[3][1] === 5) {
-    berry1.show();
-  }
-  if (map[2][6] === 5) {
-    berry2.show();
-  }
-  if (map[1][1] === 5) {
-    berry1.show();
-  }
-
-  for (let berry of berries) {
-    berry.show();
-  }
-
-  //gameplay stages
+  background(169, 169, 169);
+  safeGround();
+  startingGround();
+  endingGround();
+  water();
 
   if (gameState === "start") {
-    startScreen();
+    startscreen();
+    startButton.draw();
+
   } else if (gameState === "playing") {
-    gamePlay();
-  } else if (gameState === "gameOver") {
-    gameLose();
-    restart();
+
+    for (let i = 0; i < obstacles.length; i++) {
+      obstacles[i].update();
+      obstacles[i].draw();
+    }
+
+    for (let i = 0; i < logs.length; i++) {
+      logs[i].update();
+      logs[i].draw();
+    }
+    character1.draw();
+    character1.update();
+     drawLives();
+     drawScore();
+
   } else if (gameState === "gameSucceeded") {
     gameWin();
-    restart();
+   restartButton.draw();
+   
+  } else if (gameState === "gameOver") {
+    gameLose();
+    restartButton.draw();
   }
-}
 
-window.draw = draw;
+}
